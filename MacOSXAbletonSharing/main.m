be04bc9c462f8e61a85a02d8870859045d0b0998
Tobias Kräntzer <info@tobias-kraentzer.de>
2011-12-06 11:03:00 +0100

    Initial Commit

---
+++ MacOSXAbletonSharing/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacOSXAbletonSharing
+//
+//  Created by Tobias Kräntzer on 06.12.11.
+//  Copyright (c) 2011 nxtbgthng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
